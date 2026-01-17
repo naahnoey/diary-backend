e7d20171dfd869a3159f823995fe5e6f758f279f
naahnoey <naahyin20@sookmyung.ac.kr>
2026-01-17 18:35:34 +0900

    Initial commit: Spring Boot 다이어리 프로젝트 초기 설정

---
+++ src/test/java/com/ahy/diarybackend/DiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahy.diarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

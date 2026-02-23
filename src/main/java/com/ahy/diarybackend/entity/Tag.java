9cfacfe633a7f9c4e08957fd8f4232703877926b
naahnoey <naahyin20@sookmyung.ac.kr>
2026-02-23 09:05:39 +0900

    feat: 해시태그 관련 entity + repository

---
+++ src/main/java/com/ahy/diarybackend/entity/Tag.java
@@ -0,0 +1,31 @@
+package com.ahy.diarybackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    @Builder.Default
+    private Set<Diary> diaries = new HashSet<>();
+
+}
